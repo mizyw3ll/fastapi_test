7d83e822b87c5f0c1fb9ba6b5c164b38b605d224
kirill12342152 <sanik778345@mail.ru>
2025-11-06 17:45:08 +0300

    struct with sqlalchemy commit

--- main.py
+++ main.py
@@ -1,18 +1,70 @@
-from fastapi import FastAPI
-import uvicorn
+from typing import Annotated
+from fastapi import Depends, FastAPI
+from pydantic import BaseModel
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
 
 app = FastAPI()
 
-@app.get("/users",
-         tags=["Пользователи"],
-         summary="Получить пользователя")
-async def get_users():
-    return [{"id": 1, "name": "Кирилл"},
-            {"id": 2, "name": "Антон"}]
+engine = create_async_engine("sqlite+aiosqlite:///mydb.db", echo=True)
+
+new_async_session = async_sessionmaker(engine, expire_on_commit=False)
 
 
+async def get_session():
+    async with new_async_session() as session:
+        yield session
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000) # для докера
+
+SessionDep = Annotated[AsyncSession, Depends(get_session)]
+
+class Base(DeclarativeBase):
+    pass
 
 
+class BookModel(Base):
+    __tablename__ = "books"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str]
+    author: Mapped[str]
+
+
+class BookSchema(BaseModel):
+    title: str
+    author: str
+
+
+class BookGetSchema(BaseModel):
+    id: int
+    title: str
+    author: str
+
+
+@app.post("/setup")
+async def setup_database():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+
+
+@app.post("/books")
+async def add_book(book: BookSchema, session: SessionDep) -> BookSchema:
+    new_book = BookModel(
+        title=book.title,
+        author=book.author,
+    )
+    session.add(new_book)
+    await session.commit()
+    return book
+
+
+@app.get("/books")
+async def get_books(session: SessionDep) -> list[BookGetSchema]:
+    query = select(BookModel)
+    result = await session.execute(query)
+    books = result.scalars().all()
+    print(f"{books=}")
+    return books
